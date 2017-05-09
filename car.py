c5c7be684d67ffb6e61404233484ba7782912e80
Sequential-circuits <alcosta@alcosta.eu>
2017-05-09 12:26:20 +0200

    .

---
+++ car.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+import optparse
+from proton.handlers import MessagingHandler
+from proton.reactor import Container
+
+
+import socketserver
+import random
+import time
+from queue import Queue
+from threading import Thread
+
+# Maximum number of pending events
+MAX_QUEUE_SIZE = 100
+# Where to run the server
+HOST = '0.0.0.0'
+PORT = 9999
+
+# The time between generated events
+EVENT_PERIOD_SECONDS = 1
+# Fake data for random events
+USERS = ['user-{}'.format(i) for i in range(10)]
+EVENTS = ['login', 'purchase']
+
+class Recv(MessagingHandler):
+    def __init__(self, url, count):
+        super(Recv, self).__init__()
+        self.url = url
+        self.expected = count
+        self.received = 0
+
+    def on_start(self, event):
+        event.container.create_receiver(self.url)
+
+    def on_message(self, event):
+        if event.message.id and event.message.id < self.received:
+            # ignore duplicate message
+            return
+        if self.expected == 0 or self.received < self.expected:
+            print(event.message.body)
+            self.received += 1
+            if self.received == self.expected:
+                event.receiver.close()
+                event.connection.close()
+
+parser = optparse.OptionParser(usage="usage: %prog [options]")
+parser.add_option("-a", "--address", default="localhost:5672/examples",
+                  help="address from which messages are received (default %default)")
+parser.add_option("-m", "--messages", type="int", default=100,
+                  help="number of messages to receive; 0 receives indefinitely (default %default)")
+opts, args = parser.parse_args()
+
+
+def generate_event(users, events, delimiter=':'):
+    """Choose a random user and event, output a delimited user:event string."""
+    user = random.choice(users)
+    event = random.choice(events)
+    print ("generate_event",user,event)
+    return '{}{}{}'.format(user, delimiter, event)
+
+
+def run_server(host, port, queue):
+    """Run a SocketServer.TCPServer on host, port which writes strings from
+    queue to the socket as they become available.
+    """
+    class QueueTCPHandler(socketserver.BaseRequestHandler):
+        def handle(self):
+            while True:
+                line = queue.get()
+                self.request.sendall(str.encode(line))
+                print ("sendall",line)
+
+    server = socketserver.TCPServer((host, port), QueueTCPHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    input_queue = Queue(maxsize=MAX_QUEUE_SIZE)
+
+    server_thread = Thread(target=run_server, args=(HOST, PORT, input_queue,))
+    server_thread.daemon = True
+    server_thread.start()
+    print ("start server")
+
+    while True:
+        input_queue.put(generate_event(USERS, EVENTS) + '\n')
+        try:
+                Container(Recv(opts.address, opts.messages)).run()
+        except KeyboardInterrupt: pass
+        time.sleep(EVENT_PERIOD_SECONDS)
---
+++ roundabout.py
@@ -0,0 +1,86 @@
+from __future__ import print_function, unicode_literals
+import optparse
+from proton import Message
+from proton.handlers import MessagingHandler
+from proton.reactor import Container
+
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+import sys
+
+
+class Send(MessagingHandler):
+    def __init__(self, url, messages):
+        super(Send, self).__init__()
+        self.url = url
+        self.sent = 0
+        self.confirmed = 0
+        self.total = messages
+
+    def on_start(self, event):
+        self.acceptor = event.container.listen(self.url)
+
+    def on_sendable(self, event):
+        while event.sender.credit and self.sent < self.total:
+            msg = Message(id=(self.sent+1), body={'sequence':(self.sent+1)})
+            event.sender.send(msg)
+            self.sent += 1
+
+    def on_accepted(self, event):
+        self.confirmed += 1
+        if self.confirmed == self.total:
+            print("all messages confirmed")
+            event.connection.close()
+            self.acceptor.close()
+
+    def on_disconnected(self, event):
+        self.sent = self.confirmed
+
+parser = optparse.OptionParser(usage="usage: %prog [options]",
+                               description="Send messages to the supplied address.")
+parser.add_option("-a", "--address", default="localhost:5672/examples",
+                  help="address to which messages are sent (default %default)")
+parser.add_option("-m", "--messages", type="int", default=100,
+                  help="number of messages to send (default %default)")
+opts, args = parser.parse_args()
+
+
+
+def quiet_logs( sc ):
+  logger = sc._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel( logger.Level.ERROR )
+  logger.LogManager.getLogger("akka").setLevel( logger.Level.ERROR )
+
+def sendRecord(tup):
+    try:
+        Container(Send(opts.address, opts.messages)).run()
+    except KeyboardInterrupt: pass
+    print ("sent")
+    
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("using foreachRDD and foreach on RDD")
+    sc   = SparkContext(conf=conf)
+    ssc  = StreamingContext(sc, 2)
+    ssc.checkpoint("checkpoint")
+    
+    quiet_logs(sc)
+    
+    # Create a DStream that will connect to hostname:port, like localhost:9999
+    lines = ssc.socketTextStream("localhost", 9999)
+    # lines = ssc.textFileStream('./streamingData')
+
+    # Split each line into words
+    words = lines.flatMap(lambda line: line.split(" "))
+
+    # Count each word in each batch
+    pairs = words.map(lambda word: (word, 1))
+
+    wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+    # http://spark.apache.org/docs/latest/streaming-programming-guide.html#design-patterns-for-using-foreachrdd
+    wordCounts.foreachRDD(lambda rdd: rdd.foreach(sendRecord))
+       
+    wordCounts.pprint()
+    
+    ssc.start()             # Start the computation
+    ssc.awaitTermination()  # Wait for the computation to terminate
